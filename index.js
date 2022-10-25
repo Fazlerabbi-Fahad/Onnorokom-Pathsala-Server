a272dbccdd955017bf081cb230e869361cd029a5
Fazle Rabbi Fahad <fazlerabbifahad.54@gmail.com>
2022-10-25 19:42:45 +0600

    course page created

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ app.use(cors());
 
 
 const pages = require('./Data/pages.json');
+const courses = require('./Data/courses.json');
 
 
 app.get('/', (req, res) => {
+    res.send('pages')
+});
+
+app.get('/pages', (req, res) => {
     res.send(pages)
 });
+
+app.get('/pages/course', (req, res) => {
+    res.send(courses)
+})
+
 
 app.listen(port, () => {
     console.log(`Courses ${port}`);
